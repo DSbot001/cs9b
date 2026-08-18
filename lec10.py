1d0eafcb91005512c6e78929daf8757e0e959744
Dsbot001 <mr.dylan050805@gmail.com>
2026-08-18 01:41:45 -0700

    lecture 10-11

--- lec10.py
+++ lec10.py
@@ -16,6 +16,18 @@ class Node:
 	def setNext(self, newNext):
 		self.next = newNext
 
+
+if __name__ == "__main__":
+	a = Node(10)
+	b = Node(20)
+	c = Node(30)
+	a.next = b
+	b.next = c
+	print(a.next.data)       # 20
+	print(a.next.next.data)  # 30
+	print(c.next)            # None
+
+
 class LinkedList:
 	def __init__(self):
 		self.head = None
@@ -23,10 +35,10 @@ class LinkedList:
 	def isEmpty(self):
 		return self.head == None
 
-	def addToFront(self, item):
-		temp = Node(item)
-		temp.setNext(self.head)
-		self.head = temp
+	def addToFront(self, item):  #takes in a data value/item and adds it to the front of the list
+		temp = Node(item)        # Create a new node with the data value = item
+		temp.setNext(self.head)   # Set the new node's next to the current head of the list
+		self.head = temp     # Set the head of the list to the new node
 
 	def length(self):
 		temp = self.head
@@ -70,3 +82,5 @@ class LinkedList:
 		# Case 2: remove not 1st element
 		if found == True and previous != None:
 			previous.setNext(current.getNext())
+
+		
